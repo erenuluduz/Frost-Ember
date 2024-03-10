92fb9864869a55a308971cbcad1bb119d7ef2974
Eren Uludüz <erenuluduz43@gmail.com>
2024-03-10 23:57:35 +0300

    enemy navigation

--- Assets/_project/Runtime/Project/Test/Scripts/EnemyNavigation.cs
+++ Assets/_project/Runtime/Project/Test/Scripts/EnemyNavigation.cs
@@ -7,35 +7,14 @@ using UnityEngine.AI;
 
 public class EnemyNavigation : MonoBehaviour
 {
-    public Transform target;
-    private GameObject player;
-    private Rigidbody2D rb;
-    private NavMeshAgent enemyNavMeshAgent;
 
     public float maxDistance = 4f;
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-        rb = GetComponent<Rigidbody2D>();
-        enemyNavMeshAgent = GetComponent<NavMeshAgent>();
-
-        if (enemyNavMeshAgent == null)
-        {
-            target = player.transform;
-        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (enemyNavMeshAgent != null)
-        {
-            MoveToPlayer();
-        }
-    }
-
-    private void MoveToPlayer()
-    {
-        enemyNavMeshAgent.SetDestination(target.position);
     }
 }
--- Assets/_project/Runtime/Project/Test/Scripts/EnemyScript.cs
+++ Assets/_project/Runtime/Project/Test/Scripts/EnemyScript.cs
@@ -3,19 +3,31 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UIElements;
 
 public class EnemyScript : MonoBehaviour
 {
+    public Transform target;
+    private NavMeshAgent agent;
+
     public byte hp = 3;
     // Start is called before the first frame update
     void Start()
     {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
-    // Update is called once per frame
+    private void FixedUpdate()
+    {
+    }
     void Update()
     {
+
+        agent.SetDestination(target.position);
         #region Death
         if (hp == 0)
         {
