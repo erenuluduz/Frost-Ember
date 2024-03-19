6a05d96f01f206a28df119b59349605dba13edf3
ASLIHAN <69537329+aslihanavsar@users.noreply.github.com>
2024-03-19 13:57:37 +0300

    DoorController

---
+++ Assets/_project/Runtime/Project/Test/Scripts/DoorController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public CellController cellController; // CellController'a eriþmek için referans
+    private void Start()
+    {
+        // cellController'a referans atamasý yapýlýyor
+        cellController = GameObject.FindObjectOfType<CellController>();
+
+
+        // CellController referansý alýnmýþ mý diye kontrol et
+        if (cellController == null)
+        {
+            Debug.LogError("CellController referansý atanmamýþ!");
+            return; // Referans atanmamýþsa, kodun devamýný çalýþtýrmaya gerek yok
+        }
+    }
+
+    private void Update()
+    {
+        CheckEnemy();
+    }
+
+    private void CheckEnemy()
+    {
+        // enemyCount ve enemyGroupCount deðerlerini kontrol et
+        if (cellController.enemyCount == 0 && cellController.enemyGroupCount == 0)
+        {
+            // Kapý açýldý mesajýný yazdýr
+            Debug.Log("Kapý açýldý");
+        }
+        else
+        {
+            Debug.Log("Kapý açýlmadý. enemyCount: " + cellController.enemyCount + ", enemyGroupCount: " + cellController.enemyGroupCount);
+        }
+    }
+}
