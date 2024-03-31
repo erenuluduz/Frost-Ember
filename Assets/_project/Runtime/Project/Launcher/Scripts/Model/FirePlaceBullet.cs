8b79e48707b0e5c075cdeed065cbfb410608bcf3
SametBerkay <sametberkay04@gmail.com>
2024-03-31 14:31:23 +0300

    Hatalar var dikkat edin
    map çizildi, navmesh sorunlu, healthbar ile ilgili bir sorun veriyor

--- Assets/_project/Runtime/Project/Launcher/Scripts/Model/FirePlaceBullet.cs
+++ Assets/_project/Runtime/Project/Launcher/Scripts/Model/FirePlaceBullet.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class FirePlaceBullet : MonoBehaviour
 {
     public float speed;
+    public float destroyDelay = 5f; // Örnek olarak 3 saniye sonra yok edelim
+
     Rigidbody2D rb;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = -transform.up * speed;
+
+        // Belirli bir süre sonra yok etme iþlemi için Invoke() kullanýyoruz
+        Invoke("DestroyBullet", destroyDelay);
     }
 
-   
-    void Update()
+    void DestroyBullet()
     {
-        
+        Destroy(gameObject);
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
